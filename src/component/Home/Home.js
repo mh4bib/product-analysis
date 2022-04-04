4b5992401a17b32854cbbabf0109893b87bc78ea
mahiuddinhabib <mh.habib137@gmail.com>
2022-04-04 19:49:17 +0600

    home styled

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import HomePhoto from '../../product.jpg';
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-            <h1>Product Analysis</h1>
-            <img src={HomePhoto} alt="" />
+        <div className='home'>
+            <div className='home-desc'>
+                <h1>Product Analysis</h1>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis impedit temporibus illum dignissimos iste numquam architecto quis deserunt cupiditate nulla! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia ratione recusandae, aliquam voluptates enim maiores asperiores, itaque unde laborum accusantium dolore facere laudantium et provident nihil voluptatum, reprehenderit vitae!</p>
+            </div>
+            <div className='img'>
+                <img src={HomePhoto} alt="" />
+            </div>
         </div>
     );
 };
