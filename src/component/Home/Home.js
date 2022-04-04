4037aae927c21e6f968d9a412aca4f96266e3f5e
mahiuddinhabib <mh.habib137@gmail.com>
2022-04-04 08:25:02 +0600

    installed react router, react bootstrap, and added home compo

---
+++ src/component/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePhoto from '../../product.jpg';
+const Home = () => {
+    return (
+        <div>
+            <h1>Product Analysis</h1>
+            <img src={HomePhoto} alt="" />
+        </div>
+    );
+};
+
+export default Home;
